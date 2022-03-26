d5256892f1f69b63ae96a98a9536428ccdf39d92
Javier Rodríguez <jrguez@tibletech.com>
2022-03-26 13:13:55 +0100

    setup script adapted for dev/main net

---
+++ README.md
@@ -0,0 +1,39 @@
+# solana-validator setup for Ubuntu
+_Tested on Ubuntu 20.04 & solana-validator 1.9.14_
+_More info at [Solana Documentation - Command Line](https://docs.solana.com/cli/install-solana-cli-tools) and [Solana Documentation - Validating](https://docs.solana.com/running-validator/validator-start) _
+
+Please, **download _all files_ before executing any script**. There are several dependencies between them. You can use:
+```bash
+sudo apt install wget unzip -y
+wget https://github.com/simplyjarod/solana-validator/archive/refs/heads/main.zip
+unzip main.zip && cd solana-validator-main && rm -rf ../main.zip
+chmod u+x *.sh -R
+```
+
+## 0. Requirements
+### Hardware
+- Primary disk: 1 SSD disk (as fast as possible) of -at least- 1 TB (install OS here)
+- Secondary disk: 1 SSD disk (as fast as possible) of -at least- 500 GB
+- 256 GB RAM. A tmpfs of 300 GB will be created in RAM (with swap)
+For testing purposes (on devnet) you can use almost "any" hardware. For mainnet, at least the above requirements are needed.
+Ledger will be able to use as much as 600 GB
+
+### Network
+- Ports 8000 to 8020 open (TCP and UDP)
+- Ports 8899 and 8900 open (TCP)
+
+
+## 1. Installation and setup process
+All commands should be executed as root. Official documentation and some people recommend creating a "sol" non-root user, but some difficulties were found from my side.
+The script will prompt you for the net to use (devnet or mainnet-beta).
+
+This script will:
+- Install the [Solana Tool Suite](https://docs.solana.com/cli/install-solana-cli-tools) on its latest stable release
+- Set the [cluster](https://docs.solana.com/clusters) (network) to work on (devnet or mainnet-beta based on your choice)
+- Create the [systemd systuner.service](https://docs.solana.com/running-validator/validator-start)
+- Create the validator-start.sh script to [connect the validator](https://docs.solana.com/running-validator/validator-start)
+- Create the [systemd validator.service](https://docs.solana.com/running-validator/validator-start)
+- Create the RAMdisk for the accounts DB (to reduce SSD wear) and RAMdisk swap (to move RAM data to SSD when more RAM is needed)
+
+Please, change lines 138-139 from setup.sh accordingly to your disks configuration.
+Run `./setup.sh` as **root** from the folder this file is placed.
--- setup.sh
+++ setup.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
-# based on https://docs.solana.com/running-validator/validator-start
-# for Ubuntu 20.04
+# ARE YOU ROOT (or sudo)?
+if [[ $EUID -ne 0 ]]; then
+	echo -e "\e[91mERROR: This script must be run as root\e[0m"
+	exit 1
+fi
 
-# Execute this script as root
-cd
+cd # will be /root
 
-# Installation of Solana and selection of devnet
+# Installation of Solana and devnet choice
 sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
 solana-install update
-solana config set --url https://api.devnet.solana.com
+
+choosen_net='devnet'
+
+read -r -p "Which net would you like to use? [main/DEV] " response
+res=${response,,} # tolower
+if [[ $res =~ ^(main)$ ]]; then
+	choosen_net='mainnet-beta'
+fi
+
+if [ "$choosen_net" == "mainnet-beta" ]; then
+	solana config set --url https://api.mainnet-beta.solana.com
+else
+	solana config set --url https://api.devnet.solana.com
+fi
+
 solana transaction-count
 
 
@@ -32,25 +48,53 @@ EOF
 
 
 # Creation of validator-start.sh
+if [ "$choosen_net" == "mainnet-beta" ]; then
 cat > validator-start.sh <<EOF
 #!/bin/sh
-exec solana-validator \
-  --identity /root/validator-keypair.json \
-  --vote-account /root/vote-account-keypair.json \
-  --dynamic-port-range 8000-8020 \
-  --rpc-port 8899 \
-  --full-rpc-api \
-  --entrypoint entrypoint.devnet.solana.com:8001 \
-  --entrypoint entrypoint2.devnet.solana.com:8001 \
-  --entrypoint entrypoint3.devnet.solana.com:8001 \
-  --entrypoint entrypoint4.devnet.solana.com:8001 \
-  --entrypoint entrypoint5.devnet.solana.com:8001 \
-  --ledger /root/ledger/ \
-  --limit-ledger-size 600000000 \
-  --accounts /mnt/solana-accounts \
-  --log /root/log/solana-validator.log \
-  --no-port-check
+exec solana-validator \\
+	--identity /root/validator-keypair.json \\
+	--vote-account /root/vote-account-keypair.json \\
+	--dynamic-port-range 8000-8020 \\
+	--rpc-port 8899 \\
+	--full-rpc-api \\
+	--entrypoint entrypoint.${choosen_net}.solana.com:8001 \\
+	--entrypoint entrypoint2.${choosen_net}.solana.com:8001 \\
+	--entrypoint entrypoint3.${choosen_net}.solana.com:8001 \\
+	--entrypoint entrypoint4.${choosen_net}.solana.com:8001 \\
+	--entrypoint entrypoint5.${choosen_net}.solana.com:8001 \\
+	--known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \\
+	--known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \\
+	--known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \\
+	--known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \\
+	--only-known-rpc \\
+	--expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \\
+	--wal-recovery-mode skip_any_corrupted_record \\
+	--ledger /root/ledger-${choosen-net}/ \\
+	--limit-ledger-size 600000000 \\
+	--accounts /mnt/solana-accounts \\
+	--log /root/log/solana-validator.log \\
+	--no-port-check
 EOF
+else
+cat > validator-start.sh <<EOF
+#!/bin/sh
+exec solana-validator \\
+	--identity /root/validator-keypair.json \\
+	--vote-account /root/vote-account-keypair.json \\
+	--dynamic-port-range 8000-8020 \\
+	--rpc-port 8899 \\
+	--full-rpc-api \\
+	--entrypoint entrypoint.${choosen_net}.solana.com:8001 \\
+	--entrypoint entrypoint2.${choosen_net}.solana.com:8001 \\
+	--entrypoint entrypoint3.${choosen_net}.solana.com:8001 \\
+	--entrypoint entrypoint4.${choosen_net}.solana.com:8001 \\
+	--entrypoint entrypoint5.${choosen_net}.solana.com:8001 \\
+	--ledger /root/ledger-${choosen-net}/ \\
+	--limit-ledger-size 600000000 \\
+	--accounts /mnt/solana-accounts \\
+	--log /root/log/solana-validator.log 
+EOF
+fi
 chmod +x validator-start.sh
 mkdir log
 
@@ -71,7 +115,7 @@ LimitNOFILE=1000000
 LogRateLimitIntervalSec=0
 User=root
 Environment=PATH=/bin:/usr/bin:/root/.local/share/solana/install/active_release/bin
-Environment=SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=devnet,u=scratch_writer,p=topsecret
+Environment=SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=${choosen_net},u=scratch_writer,p=topsecret
 ExecStart=/root/validator-start.sh
 
 [Install]
@@ -113,12 +157,12 @@ echo "YOU SHOULD EXPORT/DOWNLOAD authorized-withdrawer-keypair.json NOW AND REMO
 # Config of logrotate, to avoid huge log files
 cat > /etc/logrotate.d/root <<EOF
 /root/log/solana-validator.log {
-  rotate 7
-  daily
-  missingok
-  postrotate
-    systemctl kill -s USR1 validator.service
-  endscript
+	rotate 7
+	daily
+	missingok
+	postrotate
+		systemctl kill -s USR1 validator.service
+	endscript
 }
 EOF
 systemctl restart logrotate.service
